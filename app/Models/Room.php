15dce33c2620c791868cdf051a2148a2b6e938b9
Hossam Mohammed <hossammohammed72@gmail.com>
2019-02-15 11:40:53 +0200

    models

---
+++ app/Models/Room.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function rents()
+    {
+        return $this->hasMany('App\Rent');
+    }
+
+
+}
--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function person()
+    {
+        return $this->hasOne('App\Person');
+    }
+    public function rooms()
+    {
+        return $this->hasMany('App\room');
+    }
 }
